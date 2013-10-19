584f31f3c2c9e90eee0db8aa80f2753dbd73b5f8
CS118 <cs118@ubuntu.(none)>
2013-10-19 16:00:54 -0700

    Few changes

--- ClientServer_Example/serverFork.c
+++ ClientServer_Example/serverFork.c
@@ -97,7 +97,7 @@ void dostuff (int sock)
    bzero(buffer,256);
    n = read(sock,buffer,255);
    if (n < 0) error("ERROR reading from socket");
-   printf("Here is the message: %s\n",buffer);
+   printf("Here is the message:\n %s\n",buffer);
    n = write(sock,"I got your message",18);
    if (n < 0) error("ERROR writing to socket");
 }
