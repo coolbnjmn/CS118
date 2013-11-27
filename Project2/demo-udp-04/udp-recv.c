e636c7eefb7d5a5da34f9d5522b41a7596926fae
Ajan Jayant <ajan.jayant7@gmail.com>
2013-11-27 14:56:53 -0800

    Timers added

--- Project2/demo-udp-04/udp-recv.c
+++ Project2/demo-udp-04/udp-recv.c
@@ -113,7 +113,7 @@ main(int argc, char **argv)
 			t->th_flags = 0;
 			t->th_sum = 0;
 			t->th_urp = 0;
-			alarm(7);
+
 			char* to_send = malloc(1024);
 			
 			while (count_written < f_size) {
@@ -185,4 +185,51 @@ int check_to_send(int *window, int window_size) {
 void alarm_catcher (int ignored) {
 	printf("alarm_cathcer called\n");
 	exit(1);
+}
+
+static int
+makeTimer( char *name, timer_t *timerID, int expireMS, int intervalMS )
+{
+    struct sigevent         te;
+    struct itimerspec       its;
+    struct sigaction        sa;
+    int                     sigNo = SIGRTMIN;
+
+    /* Set up signal handler. */
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = timerHandler;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(sigNo, &sa, NULL) == -1)
+    {
+        fprintf(stderr, "%s: Failed to setup signal handling for %s.\n", PROG, name);
+        return(-1);
+    }
+
+    /* Set and enable alarm */
+    te.sigev_notify = SIGEV_SIGNAL;
+    te.sigev_signo = sigNo;
+    te.sigev_value.sival_ptr = timerID;
+    timer_create(CLOCK_REALTIME, &te, timerID);
+
+    its.it_interval.tv_sec = 0;
+    its.it_interval.tv_nsec = intervalMS * 1000000;
+    its.it_value.tv_sec = 0;
+    its.it_value.tv_nsec = expireMS * 1000000;
+    timer_settime(*timerID, 0, &its, NULL);
+
+    return(0);
+}
+
+static void
+timerHandler( int sig, siginfo_t *si, void *uc )
+{
+    timer_t *tidp;
+    tidp = si->si_value.sival_ptr;
+
+    if ( *tidp == firstTimerID )
+        firstCB(sig, si, uc);
+    else if ( *tidp == secondTimerID )
+        secondCB(sig, si, uc);
+    else if ( *tidp == thirdTimerID )
+        thirdCB(sig, si, uc);
 }
